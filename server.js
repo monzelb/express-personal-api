e72e967d9307ef59408de6b82c9f8897a4c713eb
monzelb <monzelb@gmail.com>
2017-02-10 15:50:25 -0800

    added profile endpoint

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ app.use(function(req, res, next) {
 // i.e. `/images`, `/scripts`, `/styles`
 app.use(express.static('public'));
 
+
+var profile= [{name: "Brett Monzel",
+              githubLink: "https://github.com/monzelb",              
+              personalSiteLink: "https://monzelb.github.io/",
+              cityOfOrigin: "Cincinnati, Ohio",
+              currentCity: "San Francisco, California"
+              }]
 /*
  * HTML Endpoints
  */
@@ -47,15 +54,25 @@ app.get('/api', function apiIndex(req, res) {
   // It would be seriously overkill to save any of this to your database.
   res.json({
      // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    message: "Welcome to my photography api! Here's what you need to know!",
+    documentationUrl: "https://github.com/monzelb/express-personal-api/blob/master/README.md", // CHANGE ME
+    baseUrl: "https://powerful-brushlands-77187.herokuapp.com/", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Important facts about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "Campsites I've been too"} // CHANGE ME
+      {method: "POST", path: "/api/photography", description: "Photographs I've taken"} // CHANGE ME
     ]
   })
+});
+
+app.get('/api/profile', function profile(req, res){
+   res.json(
+            {name: "Brett Monzel",
+              githubLink: "https://github.com/monzelb",              
+              personalSiteLink: "https://monzelb.github.io/",
+              cityOfOrigin: "Cincinnati, Ohio",
+              currentCity: "San Francisco, California",
+              })
 });
 
 /**********
