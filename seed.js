bec94bf56dcf6ccbfca1e4ab3d26014affcffd30
monzelb <monzelb@gmail.com>
2017-02-11 12:30:14 -0800

    added code for seed.js

--- seed.js
+++ seed.js
@@ -18,22 +18,88 @@ var db = require('./models');
 
 var photo_list = [
   {
-  id: 1,
   location: "San Francisco",
   image: "https://raw.githubusercontent.com/monzelb/monzelb.github.io/master/assets/imgs/ocean.jpg",
-  contains: "Ocean"
+  contents: "ocean"
   },
   {
-  id: 2,
   location: "Canada",
   image: "",
-  contains: "Mountains"
+  contents: "mountains"
   },
   {
-  id: 3,
   location: "Mt. Shasta",
   image: "",
-  contains: "mountains"
+  contents: "mountains"
   },
  
 ];
+
+var location_list= [
+  {
+   name: "San Francisco"
+  },
+  {
+   name: "British Columbia"
+  }
+]
+
+var content_list = [
+   {
+   name: "ocean"
+   },
+   {
+   name: "mountains"
+   },
+   {
+   name: "city"
+   }
+]
+
+db.Location.remove({}, function(err, locations) {
+  console.log('removed all photos');
+  db.Location.create(location_list, function(err, locations){
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('recreated all locations');
+    console.log("created", locations.length, "locations");
+
+    db.Contents.remove({}, function(err, contents) {
+  console.log('removed all contents');
+  db.Contents.create(content_list, function(err, contents){
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('recreated all contents');
+    console.log("created", contents.length, "contents");
+
+
+    db.Photo.remove({}, function(err, photos){
+      console.log('removed all photos');
+      photos_list.forEach(function (photoData) {
+        var photo = new db.Photo({
+          image: photoData.image,
+          contents: photoData.contents
+        });
+        db.Photo.findOne({name: photoData.location}, function (err, foundLocation) {
+          console.log('found location ' + foundLocation.name + ' for photo ');
+          if (err) {
+            console.log(err);
+            return;
+          }
+          photo.location = foundLocation;
+          photo.save(function(err, savedPhoto){
+            if (err) {
+              return console.log(err);
+            }
+            console.log('saved ' + savedPhoto.title + ' of ' + foundLocation.name);
+          });
+        });
+      });
+    });
+
+  });
+});
